d29253ed74f89ffad9f53635a38135fcff24d20e
LucasFsc <lucas.fscr@gmail.com>
2022-09-20 15:42:04 -0300

    add graphql definition file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common'
-import { APP_GUARD } from '@nestjs/core'
-import { MongooseModule } from '@nestjs/mongoose'
-import { ConfigModule } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
-import { join } from 'path'
-import { UsersModule } from './users/users.module'
-import { AuthModule } from './auth/auth.module'
-import { GraphQLError } from 'graphql'
 import { GqlJwtAuthGuard } from '@/guards/gql-jwt-auth.guard'
 import { RolesGuard } from '@/guards/roles.guard'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+import { GraphQLModule } from '@nestjs/graphql'
+import { MongooseModule } from '@nestjs/mongoose'
+import { GraphQLError } from 'graphql'
+import { join } from 'path'
+import { AuthModule } from './auth/auth.module'
 import { CapesModule } from './capes/capes.module'
 import { RubberDucksModule } from './rubber-ducks/rubber-ducks.module'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
@@ -19,7 +19,12 @@ import { RubberDucksModule } from './rubber-ducks/rubber-ducks.module'
       useNewUrlParser: true
     }),
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/graphql.schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+        skipResolverArgs: true
+      },
       sortSchema: true,
       formatError: (error: GraphQLError) => ({
         message: error.extensions?.exception?.response?.message || error.message
